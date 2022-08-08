556aa6eb8c22c66bf988c2e3954b09e3dd0d71c7
Paul DeSalvo <65404809+prdesalvo@users.noreply.github.com>
2022-08-08 10:51:01 +0000

    Added additional loading text

--- static/script.js
+++ static/script.js
@@ -1,7 +1,23 @@
 $("button").click(function(){
   // $("#spinner").removeClass('d-none');
-  $("#test").addClass('d-none');
+  $("#find_places").addClass('d-none');
   $("#spinner").removeClass("d-none");
+
+  setTimeout(function() {
+    $("#first_p").slideDown('slow');
+  }, 1000)
+
+  setTimeout(function() {
+    $("#second_p").slideDown("slow");
+  }, 2000)
+
+  setTimeout(function() {
+    $("#third_p").slideDown("slow");
+  }, 3000)
+
+  setTimeout(function() {
+    $("#fourth_p").slideDown("slow");
+  }, 4000)
   
 });
 
