74732bbbae16a1079bf60d15a83c30efc41b0e5c
Paul DeSalvo <65404809+prdesalvo@users.noreply.github.com>
2022-08-09 11:54:37 +0000

    cleaned up javascript and using delay method

--- static/script.js
+++ static/script.js
@@ -1,27 +1,11 @@
 $("button").click(function(){
-  // $("#spinner").removeClass('d-none');
+  
   $("#find_places").addClass('d-none');
   $("#spinner").removeClass("d-none");
 
-  setTimeout(function() {
-    $("#first_p").slideDown('slow');
-  }, 1000)
-
-  setTimeout(function() {
-    $("#second_p").slideDown("slow");
-  }, 2000)
-
-  setTimeout(function() {
-    $("#third_p").slideDown("slow");
-  }, 3000)
-
-  setTimeout(function() {
-    $("#fourth_p").slideDown("slow");
-  }, 4000)
+  $("#first_p").delay(1000).slideDown('slow');
+  $("#second_p").delay(2000).slideDown("slow");
+  $("#third_p").delay(3000).slideDown("slow");
+  $("#fourth_p").delay(4000).slideDown("slow");
   
-});
-
-$('.nav-link').click(function(){
-  $(".active").removeClass('active')
-  // $(this).addClass('active')
 });
