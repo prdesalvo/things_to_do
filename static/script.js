e43fafe90119978d1cb86cad415e25dff87c993d
Paul DeSalvo <65404809+prdesalvo@users.noreply.github.com>
2022-08-19 22:12:40 +0000

    Figured out jquery append. I had conflicting quotes

--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 $("form").submit(function(){
-  $("#find_places").prop("disabled",true).text('Loading...')
-  // $("#find_places").prepend("<span class="spinner-border spinner-border-sm d-none" role="status" aria-hidden="true"></span>");
+  $("#find_places").prop("disabled",true).text(' Loading...').prepend('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>');
 
   $("#first_p").delay(1000).slideDown('slow');
   $("#second_p").delay(2000).slideDown("slow");
